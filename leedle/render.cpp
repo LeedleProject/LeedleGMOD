2fb598e9ac45e18c7a33aeab106acdb41a25d455
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2023-03-10 19:12:48 +0300

    imgui

--- leedle/render.cpp
+++ leedle/render.cpp
@@ -8,6 +8,8 @@
 #include <loguru.hpp>
 #include <magic_enum.hpp>
 
+#include <imgui_impl_win32.h>
+#include <imgui_impl_dx9.h>
 
 using namespace render;
 
@@ -27,7 +29,31 @@ long Render::end_scene_callback(IDirect3DDevice9* device) {
         LOG_S(INFO) << "Device: " << std::hex << device << std::endl;
 
         ImGui::CreateContext();
+        if (auto hwnd = FindWindowW(L"Valve001", 0); hwnd) {
+            ImGui_ImplWin32_Init(hwnd);
+            ImGui_ImplDX9_Init(device);
+            ImGui::GetIO().IniFilename = nullptr;
+
+            static const ImWchar ranges[] = {
+				0x0020, 0x00FF, // Basic Latin + Latin Supplement
+				0x0400, 0x044F, // Cyrillic
+				0,
+			};
+            ImGui::GetIO().Fonts->AddFontFromFileTTF("C:/Windows/Fonts/L_10646.ttf", 18.f, 0, ranges);
+        }
     }
+
+    ImGui_ImplDX9_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Begin("Hello, world!");
+
+    ImGui::End();
+
+    ImGui::EndFrame();
+    ImGui::Render();
+    ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
 
     return end_scane_hook.vmt_hook.original(device);
 }
